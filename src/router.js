3de51dcd8074da3f1544c0335436da2712f4ea6d
yangxin2017 <dklongqishi@163.com>
2023-04-25 13:36:26 +0800

    初始化

---
+++ src/router.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+Vue.use(Router)
+export default new Router({
+  mode: 'hash',
+  base: '/',
+  routes: [
+    {
+      path: '/',
+      component: () => import('./views/Home'),
+    },
+    {
+      path: '/login',
+      component: () => import('./views/Home'),
+    },
+  ],
+})
